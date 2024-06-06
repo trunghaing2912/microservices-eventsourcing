ea53078dba776970b1dc5bd5dacfad3fbcc3275a
Hai Nguyen Trung (VTI.DG) <hai.nguyentrung@vti.com.vn>
2024-06-06 10:30:44 +0700

    api gateway

--- api-gateway/src/test/java/com/example/api_gateway/ApiGatewayApplicationTests.java
+++ api-gateway/src/test/java/com/example/api_gateway/ApiGatewayApplicationTests.java
@@ -1,10 +1,10 @@
-package com.tanthanh.apigateway;
+package com.example.api_gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApigatewayApplicationTests {
+class ApiGatewayApplicationTests {
 
 	@Test
 	void contextLoads() {
